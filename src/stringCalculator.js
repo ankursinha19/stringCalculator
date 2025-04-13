cd4427041b2298b8eb4c1d16541f012c8b5171ce
ankursinha19 <ankursinha197@gmail.com>
2025-04-13 22:55:28 +0530

    function : support custom delimiters defined in input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -23,13 +23,31 @@
 //   return numbers.reduce((sum, num) => sum + num, 0);
 // }
 
+// function add(input) {
+//   if (input === '') return 0;
+//   const delimiterRegex = /[\n,]/; // Matches either comma or newline
+//   const numbers = input.split(delimiterRegex).map(Number);
+//   return numbers.reduce((sum, num) => sum + num, 0);
+// }
+
 function add(input) {
   if (input === '') return 0;
-  const delimiterRegex = /[\n,]/; // Matches either comma or newline
+
+  // Check for custom delimiter definition at the start
+  if (input.startsWith('//')) {
+    const delimiterSection = input.split('\n')[0];
+    const delimiter = delimiterSection.slice(2); // Extract delimiter
+    input = input.slice(delimiterSection.length + 1); // Remove delimiter definition from input
+    const delimiterRegex = new RegExp(`[${delimiter}\n,]`); // Support custom delimiter along with comma and newline
+    const numbers = input.split(delimiterRegex).map(Number);
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
+
+  // Default case for commas and newlines
+  const delimiterRegex = /[\n,]/;
   const numbers = input.split(delimiterRegex).map(Number);
   return numbers.reduce((sum, num) => sum + num, 0);
 }
-
 
 
 
