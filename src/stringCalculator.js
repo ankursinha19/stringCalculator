d03a28146e885c0431570182ef138baa08c23bc4
ankursinha19 <ankursinha197@gmail.com>
2025-04-13 22:53:20 +0530

    function  supports newlines as delimiters along with commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -17,9 +17,16 @@
 //   return numbers.reduce((sum, num) => sum + num, 0);
 // }
 
+// function add(input) {
+//   if (input === '') return 0;
+//   const numbers = input.split(',').map(Number);
+//   return numbers.reduce((sum, num) => sum + num, 0);
+// }
+
 function add(input) {
   if (input === '') return 0;
-  const numbers = input.split(',').map(Number);
+  const delimiterRegex = /[\n,]/; // Matches either comma or newline
+  const numbers = input.split(delimiterRegex).map(Number);
   return numbers.reduce((sum, num) => sum + num, 0);
 }
 
