cf054ce6b717e13cb605b266bfd365c02333028b
ankursinha19 <ankursinha197@gmail.com>
2025-04-13 22:47:05 +0530

    function eturn sum of two comma-separated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,8 +5,24 @@
 //   module.exports = { add };
   
 
+// function add(input) {
+//   if (input === '') return 0;
+//   return parseInt(input, 10);
+// }
+
+
 function add(input) {
   if (input === '') return 0;
-  return parseInt(input, 10);
+  const numbers = input.split(',').map(Number);
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
+
+
+
+
+
+
+
+
+
 module.exports = { add };
