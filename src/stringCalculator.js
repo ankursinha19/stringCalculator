3876cda84ec31c608834bf06a935e8555cea5d89
ankursinha19 <ankursinha197@gmail.com>
2025-04-13 22:49:46 +0530

    feat: support sum of multiple comma-separated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -11,12 +11,17 @@
 // }
 
 
+// function add(input) {
+//   if (input === '') return 0;
+//   const numbers = input.split(',').map(Number);
+//   return numbers.reduce((sum, num) => sum + num, 0);
+// }
+
 function add(input) {
   if (input === '') return 0;
   const numbers = input.split(',').map(Number);
   return numbers.reduce((sum, num) => sum + num, 0);
 }
-
 
 
 
