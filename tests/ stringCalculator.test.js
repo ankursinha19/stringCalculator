51e75bba9c81a500053b84df485b2b32bad411a1
ankursinha19 <ankursinha197@gmail.com>
2025-04-13 22:42:01 +0530

    test: returns number when a single number is passed

--- tests/ stringCalculator.test.js
+++ tests/ stringCalculator.test.js
@@ -1,5 +1,11 @@
-const { add } = require('../src/stringCalculator');
+// const { add } = require('../src/stringCalculator');
 
-test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+// test('returns 0 for an empty string', () => {
+//   expect(add("")).toBe(0);
+// });
+
+
+test('returns number when a single number is passed', () => {
+  const result = add('1');
+  expect(result).toBe(1);
 });
